85f1f98e2af3a3a0060f3b1d99c6c5b210ecf2be
James Adrian <jamesadrianandog@gmail.com>
2026-03-30 11:26:59 +0800

    fixed issues on larastan and phpunit

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -9,8 +9,10 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    public function test_application_name_is_string(): void
     {
-        $this->assertTrue(true);
+        $value = config('app.name');
+
+        $this->assertIsString($value);
     }
 }
